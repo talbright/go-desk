eb8d4fd90b218137fe7505b89149d2d15bcd4491
Trent Albright <talbright@desk.com>
2014-06-03 08:38:37 -0400

    Test for Restful type

--- desk/restful_test.go
+++ desk/restful_test.go
@@ -6,9 +6,47 @@ import (
   . "github.com/smartystreets/goconvey/convey"
 )
 
-func RestfulMessage(t *testing.T) {
+func TestRestful(t *testing.T) {
   fmt.Println("")
-  SkipConvey("TODO", t, func() {
+  Convey("Post", t, func() {
+    Convey("should build fields",func(){
+      r:=Restful{}
+      r.Post("/path")
+      So(r.path,ShouldEqual,"/path")
+      So(r.method,ShouldEqual,"POST")
+    })
+  })
+  Convey("Get", t, func() {
+    Convey("should build fields",func(){
+      r:=Restful{}
+      r.Get("/path")
+      So(r.path,ShouldEqual,"/path")
+      So(r.method,ShouldEqual,"GET")
+    })
+  })
+  Convey("Put", t, func() {
+    Convey("should build fields",func(){
+      r:=Restful{}
+      r.Put("/path")
+      So(r.path,ShouldEqual,"/path")
+      So(r.method,ShouldEqual,"PUT")
+    })
+  })
+  Convey("Patch", t, func() {
+    Convey("should build fields",func(){
+      r:=Restful{}
+      r.Patch("/path")
+      So(r.path,ShouldEqual,"/path")
+      So(r.method,ShouldEqual,"PATCH")
+    })
+  })
+  Convey("Delete", t, func() {
+    Convey("should build fields",func(){
+      r:=Restful{}
+      r.Delete("/path")
+      So(r.path,ShouldEqual,"/path")
+      So(r.method,ShouldEqual,"DELETE")
+    })
   })
 }
 
